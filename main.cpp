132b69536bf35390a72d71aeddaff5a9ce8e3593
Adarsh Baghel <adarsh@appointy.com>
2020-11-21 22:02:13 +0530

    added code files

---
+++ main.cpp
@@ -0,0 +1,92 @@
+/*
+* CSE 180001001 
+* Adarsh Baghel
+* Assignment 4
+*/
+
+#include <mpi.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+double *createMatrix(int n,int m){
+    double *matrix;
+    matrix = new double[n*m];
+
+    for (int h=0; h<n*m; h++) {
+        matrix[h] = h+1;
+    }
+
+    return matrix;
+}
+
+int main(int argc, char *argv[])
+{
+	int pid, nProc, ierr;
+	double mytime, maxtime, mintime, avgtime;
+	ierr = MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    
+    //#####################################
+
+    double infNorm;
+    int m=8,n=8;
+    double *a;
+    
+    if(pid==0){
+    	cin>>m>>n;
+        assert(m>=nProc);
+        a = createMatrix(m,n);
+        if(false){
+            a = new double[m*n];
+        	for(int i=0; i<m; i++){
+                for(int j=0; j<n; j++)
+            		cin>>a[i*m+j];
+            }
+        }
+    }
+
+    mytime = MPI_Wtime(); /*get time just before work section */
+    MPI_Bcast(&n, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&m, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double data[m/nProc][n];
+    int size=(n*m)/nProc;
+
+    MPI_Scatter(a,size ,MPI_DOUBLE, data, size, MPI_DOUBLE, 0, MPI_COMM_WORLD); //Scatter the Matrix
+    
+
+    double rowSum=0;
+    for(int i=0; i<m/nProc; i++){
+        //cout<<"PID :"<<pid<<" :";
+        double sum=0;
+        for(int j=0; j<n; j++){
+            //cout<<data[i][j]<<" ";
+            sum+=abs(data[i][j]);
+        }
+        //cout<<endl;
+        rowSum=max(sum,rowSum);
+    }
+
+    MPI_Reduce(&rowSum, &infNorm, 1, MPI_DOUBLE, MPI_MAX, 0,MPI_COMM_WORLD);
+
+    if (pid == 0) {
+        printf("INF Norm: %lf\n", infNorm);
+    }
+
+	//#####################################
+
+    mytime = MPI_Wtime() - mytime; /*get time just after work section*/
+    MPI_Reduce(&mytime, &maxtime, 1, MPI_DOUBLE,MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&mytime, &mintime, 1, MPI_DOUBLE, MPI_MIN, 0,MPI_COMM_WORLD);
+	MPI_Reduce(&mytime, &avgtime, 1, MPI_DOUBLE, MPI_SUM, 0,MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (pid == 0) 
+   	{
+  		avgtime /= nProc;
+		printf("Min: %lf Max: %lf Avg: %lf\n", mintime, maxtime, avgtime);
+    }
+
+    MPI_Finalize();
+	return 0;
+}
